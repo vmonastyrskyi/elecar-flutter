857b0992109f2068adb66d54a3a11e7a968b7e4a
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2022-04-02 19:32:31 +0300

    Add application startup splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const ElecarApp());
 }
 
-class ElecarApp extends StatelessWidget {
+class ElecarApp extends StatefulWidget {
   const ElecarApp({Key? key}) : super(key: key);
+
+  @override
+  State<ElecarApp> createState() => _ElecarAppState();
+}
+
+class _ElecarAppState extends State<ElecarApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
