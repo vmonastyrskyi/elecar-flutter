4fc6a60f1d2d33dab20b78c4c493db9fef6e0d65
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2022-04-02 20:16:50 +0300

    Add application color set

---
+++ lib/app_colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color transparent = Color(0x00000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+
+  static const Color bodyColor = Color(0xFF0A0A0B);
+  static const Color containerColor = Color(0xFF111213);
+
+  static const Color titleColor = Color(0xFFF1F2F3);
+  static const Color textColor = Color(0xFFBABEC4);
+  static const Color textColorLight = Color(0xFF888B91);
+
+  static const Color primaryColor = Color(0xFF4177DC);
+  static const Color primaryColorAlt = Color(0xFF306AD9);
+  static const Color secondaryColor = Color(0xFF608CE2);
+  static const Color secondaryColorAlt = Color(0xFF93B1EB);
+}
