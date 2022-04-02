32617cb4969ab1fee0a8abf71c2e4ceedb457a4a
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2022-04-02 21:53:56 +0300

    Add svg & image preloader

---
+++ lib/app_resources.dart
@@ -0,0 +1,3 @@
+abstract class AppResources {
+  static const String logo = 'assets/images/logo.svg';
+}
---
+++ lib/resources_preloader.dart
@@ -0,0 +1,8 @@
+import 'app_resources.dart';
+import 'utils/preload_svg.dart';
+
+Future<void> preloadResources() async {
+  await Future.wait([
+    preloadSvg(AppResources.logo),
+  ]);
+}
