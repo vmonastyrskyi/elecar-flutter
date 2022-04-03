79388531c186467b8be89ec6882fb63c774eb66f
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2022-04-03 19:54:37 +0300

    Add resources

--- lib/app_resources.dart
+++ lib/app_resources.dart
@@ -1,4 +1,9 @@
 abstract class AppResources {
   static const String logo = 'assets/images/logo.svg';
   static const String menu = 'assets/images/menu.svg';
+  static const String temperature = 'assets/images/temperature.svg';
+  static const String mileage = 'assets/images/mileage.svg';
+  static const String battery = 'assets/images/battery.svg';
+
+  static const String carHome = 'assets/images/car_home.png';
 }
--- lib/resources_preloader.dart
+++ lib/resources_preloader.dart
@@ -1,3 +1,6 @@
+import 'package:elecar/utils/preload_image.dart';
+import 'package:flutter/cupertino.dart';
+
 import 'app_resources.dart';
 import 'utils/preload_svg.dart';
 
@@ -5,5 +8,12 @@ Future<void> preloadResources() async {
   await Future.wait([
     preloadSvg(AppResources.logo),
     preloadSvg(AppResources.menu),
+    preloadSvg(AppResources.temperature),
+    preloadSvg(AppResources.mileage),
+    preloadSvg(AppResources.battery),
+  ]);
+
+  await Future.wait([
+    preloadImage(const AssetImage(AppResources.carHome)),
   ]);
 }
