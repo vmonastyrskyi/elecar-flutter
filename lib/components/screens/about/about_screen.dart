794932fe02d2ee1a0dc17dba5f87e693e777277b
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2022-04-08 22:30:44 +0300

    Add about image description

--- lib/components/screens/about/about_screen.dart
+++ lib/components/screens/about/about_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:elecar/app_colors.dart';
 import 'package:elecar/app_resources.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +11,7 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SingleChildScrollView(
-        padding: const EdgeInsets.fromLTRB(24.0, 64.0, 24.0, 64.0),
+        padding: const EdgeInsets.fromLTRB(24.0, 56.0, 24.0, 56.0),
         physics: const BouncingScrollPhysics(),
         child: Column(
           children: [
@@ -21,12 +24,65 @@ class AboutScreen extends StatelessWidget {
   }
 
   Widget _buildAboutImage() {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(8.0),
-      child: Image.asset(
-        AppResources.about,
-        fit: BoxFit.cover,
-      ),
+    return Stack(
+      alignment: Alignment.topCenter,
+      clipBehavior: Clip.none,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Image.asset(
+            AppResources.about,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          right: 0.0,
+          bottom: -50.0,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                width: 200.0,
+                height: 100.0,
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: AppColors.white.withOpacity(0.1),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Text(
+                      '2.500+',
+                      style: TextStyle(
+                        height: 1.4,
+                        fontSize: 16.0,
+                        fontFamily: 'Exo',
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'Supercharges placed along\npopular routes',
+                      style: TextStyle(
+                        height: 1.4,
+                        fontSize: 12.0,
+                        fontFamily: 'Exo',
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
